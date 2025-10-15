f94e308e76a4507a8448fa3533b3688a6b4f0be0
RITZ <ritz1014.ritz1014@gmail.com>
2025-10-15 12:33:28 +0900

    add: popup

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,26 @@
 import { defineConfig } from 'vite'
+import { join, resolve } from 'path'
 import react from '@vitejs/plugin-react'
-import { crx } from '@crxjs/vite-plugin'
-import manifest from './manifest.json'
-
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    crx({ manifest })
-  ],
+  root: resolve(__dirname, 'src'),
+  publicDir: resolve(__dirname, 'public'),
+  build: {
+    outDir: resolve(__dirname, 'dist'),
+    rollupOptions: {
+      input: {
+        popup: join(__dirname, 'src/popup/index.html'),
+      },
+      output: {
+        entryFileNames: 'assets/[name].js'
+      },
+    }
+  },
+  resolve: {
+    alias: {
+      '@src': resolve(__dirname, 'src')
+    }
+  }, 
+    plugins: [react()],
 })
